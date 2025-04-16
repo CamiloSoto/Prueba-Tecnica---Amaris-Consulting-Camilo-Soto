5be149e16d1d603bb3efcffaa5649fad191f3918
Camilo Soto <olimpusmac@gmail.com>
2025-04-16 11:38:03 -0500

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.funds_routes import router as funds_routes
 
 app = FastAPI(title="Gestión de Fondos")
 
-app.include_router(funds_routes, prefix="/fondos", tags=["Fondos"])
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+app.include_router(funds_routes, prefix="/funds", tags=["Funds"])
 
 @app.get("/")
 def root():
