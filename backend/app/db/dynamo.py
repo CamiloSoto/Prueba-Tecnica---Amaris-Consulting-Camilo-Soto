82df2060c57da8a59194f19e879061486215faa7
Camilo Soto <olimpusmac@gmail.com>
2025-04-16 10:39:24 -0500

    dynamo.py

---
+++ backend/app/db/dynamo.py
@@ -0,0 +1,16 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dynamodb = boto3.resource(
+    "dynamodb",
+    region_name="us-east-1",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+)
+
+fondos_table = dynamodb.Table("Fondos")
+transacciones_table = dynamodb.Table("Transacciones")
+clientes_table = dynamodb.Table("Clientes")
