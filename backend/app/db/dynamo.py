23b4452cac2ec809a196f47ac144ae7eb9b7914a
Camilo Soto <olimpusmac@gmail.com>
2025-04-16 10:40:59 -0500

    ...

--- backend/app/db/dynamo.py
+++ backend/app/db/dynamo.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 dynamodb = boto3.resource(
     "dynamodb",
-    region_name="us-east-1",
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
 )
